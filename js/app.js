8e37616409417c9bfb8c2cc2cb53de0a994aeea3
Your Name <you@example.com>
2022-07-14 17:55:52 -0600

    jsdom knowledgecheck wk10 wip

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let head_h1 = document.getElementById(`header_h1`);
+head_h1[`innerText`] = `haha im now the first header`;
+
+let all_h2 = document.getElementsByClassName(`h2_gang`);
+all_h2[`innerText`] = `h2 gang rise up`;
+for (let i = 0; i < all_h2; i++);
