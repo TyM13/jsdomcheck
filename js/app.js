cd799012ec4056ecb21fef7ae0e664b43c4dcf0f
Your Name <you@example.com>
2022-07-14 18:59:19 -0600

    jsdom knowledge check pt2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-let head_h1 = document.querySelector(`#header_h1`);
-head_h1[`innerText`] = `haha im now the first header`;
+// let head_h1 = document.querySelector(`#header_h1`);
+// head_h1[`innerText`] = `haha im now the first header`;
 
-let all_h2 = document.querySelectorAll(`h2`);
-for (let i = 0; i < all_h2.length; i++){
-all_h2[i][`innerText`] = `h2 gang rise up`;
+// let all_h2 = document.querySelectorAll(`.h2`);
+// for (let i = 0; i < all_h2.length; i++){
+// all_h2[i][`innerText`] = `h2 gang rise up`;
+// }
+
+
+
+// let main_ptwo = document.getElementById(`main_ptag_two`);
+// main_ptwo[`innerText`] =`Maaiinn Ptag 2!`
+
+// let main_h3 = document.getElementById(`main_h3`);
+// main_h3[`innerText`] =`Maaiinn H3 ending!!`
+
+
+let all_h4 = document.getElementsByClassName(`h4_class`);
+for (let i = 0; i < all_h4.length; i++){
+all_h4[i][`innerText`] = `h4 tags are best tags`;
 }
